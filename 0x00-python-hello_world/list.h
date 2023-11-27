ea544783513e1be8468e0851400a00d1b8502f00
Locozyoungg <collaustin27@gmail.com>
2023-11-27 09:38:27 -0800

    first_commit

---
+++ 0x00-python-hello_world/list.h
@@ -0,0 +1,6 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+void print_best_school(void);
+
+#endif /* LISTS_H */
---
+++ 0x00-python-hello_world/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def print_best_school():
+    """
+    Prints 'Best School'.
+    """
+    print("Best School")
+
+if __name__ == "__main__":
+    print_best_school()
