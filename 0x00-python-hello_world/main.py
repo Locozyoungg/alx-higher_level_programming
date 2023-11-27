ceefcc0eb7781395ba3c96cd223dda0476e923fc
Locozyoungg <collaustin27@gmail.com>
2023-11-27 09:49:58 -0800

    fix_2

--- 0x00-python-hello_world/main.py
+++ 0x00-python-hello_world/main.py
@@ -1,10 +1,2 @@
 #!/usr/bin/python3
-
-def print_best_school():
-    """
-    Prints 'Best School'.
-    """
-    print("Best School")
-
-if __name__ == "__main__":
-    print_best_school()
+print("Best School")
