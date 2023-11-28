7f22ef821ff8c574ad1beb373078442cbc5371d4
Locozyoungg <collaustin27@gmail.com>
2023-11-28 13:33:44 -0800

    last_print1

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+k#!/usr/bin/python3
 
 def print_last_digit(number):
     last_digit = abs(number) % 10
@@ -9,4 +9,5 @@ def print_last_digit(number):
 print_last_digit(98)
 print_last_digit(0)
 r = print_last_digit(-1024)
+print()
 print(r)
