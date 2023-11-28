adbbfc93f771579d964a06b798f4c7fdf1d565bd
Locozyoungg <collaustin27@gmail.com>
2023-11-28 14:55:16 -0800

    tebah

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+# Author - Godswill Kalu
 
-for i in range(ord('z'), ord('A') - 1, -1):
-    print("{:c}".format(i) if i % 2 == 0 else "{:c}".format(i - 32), end="")
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0
