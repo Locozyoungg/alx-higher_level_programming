66549ae9e45401449ea97385eeec0e17665ca21a
Locozyoungg <collaustin27@gmail.com>
2023-12-03 14:12:10 -0800

    new_list

--- 0x03-python-data_structures/4-main.py
+++ 0x03-python-data_structures/4-main.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-new_in_list = __import__('4-new_in_list').new_in_list
 
-my_list = [1, 2, 3, 4, 5]
-idx = 3
-new_element = 9
-new_list = new_in_list(my_list, idx, new_element)
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list.copy()
 
-print(new_list)
-print(my_list)
+    copy = my_list.copy()
+    copy[idx] = element
+    return copy
+
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    idx = 3
+    new_element = 9
+    new_list = new_in_list(my_list, idx, new_element)
+
+    print(new_list)
+    print(my_list)
