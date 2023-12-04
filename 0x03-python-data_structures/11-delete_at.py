87c9ef2efba241429345ebb47be0ec34bd605ec0
Locozyoungg <collaustin27@gmail.com>
2023-12-03 16:03:27 -0800

    delete

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -5,6 +5,6 @@ def delete_at(my_list=[], idx=0):
     if idx < 0 or idx >= len(my_list):
         return my_list
 
-    # Create a new list without the element at the specified index
-    new_list = my_list[:idx] + my_list[idx + 1:]
-    return new_list
+    # Delete the element at the specified index in the original list
+    del my_list[idx]
+    return my_list
