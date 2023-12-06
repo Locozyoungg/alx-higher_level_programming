2a4c7603f99525d381abbe6e2b48d0590e791c47
Locozyoungg <collaustin27@gmail.com>
2023-12-06 14:06:52 -0800

    weight

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+def weight_average(my_list=[]):
+    """Return the weighted average of integers in a tuple (<score>, <weight>)."""
+    if not my_list:
+        return 0
+
+    total_score = 0
+    total_weight = 0
+
+    for score, weight in my_list:
+        total_score += score * weight
+        total_weight += weight
+
+    if total_weight == 0:
+        return 0
+
+    return total_score / total_weight
