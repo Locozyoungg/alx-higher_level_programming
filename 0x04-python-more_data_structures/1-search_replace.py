3e8a9a73f50efc2faeb897d0158b584d17eb33e6
Locozyoungg <collaustin27@gmail.com>
2023-12-06 11:08:21 -0800

    search_replc

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,6 +2,17 @@
 
 def search_replace(my_list, search, replace):
     """Replace all occurrences of an element with another in a new list."""
-    # Create a new list using list comprehension with replacement
-    new_list = [replace if element == search else element for element in my_list]
+    # Create an empty list to store the new elements
+    new_list = []
+
+    # Iterate over each element in the original list
+    for element in my_list:
+        # Check if the current element is equal to the search element
+        if element == search:
+            # If yes, append the replace element to the new list
+            new_list.append(replace)
+        else:
+            # If no, append the current element to the new list
+            new_list.append(element)
+
     return new_list
