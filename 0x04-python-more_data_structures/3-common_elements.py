4ca1835c80cb1da4c7779d0ad6abda1a54386f2b
Locozyoungg <collaustin27@gmail.com>
2023-12-06 11:31:03 -0800

    common_elemnt

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def common_elements(set_1, set_2):
+    """Return a set of common elements in two sets."""
+    # Use the intersection method to find common elements
+    common_set = set_1.intersection(set_2)
+    return common_set
