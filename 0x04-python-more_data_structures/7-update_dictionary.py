dfed4601f403a33ebe6fa01f26de9c2bdb62e871
Locozyoungg <collaustin27@gmail.com>
2023-12-06 12:13:47 -0800

    dictinary_update

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,6 +2,5 @@
 
 def update_dictionary(a_dictionary, key, value):
     """Replace or add key/value in a dictionary."""
-    # Update the value if the key exists, otherwise, create a new key-value pair
     a_dictionary[key] = value
     return a_dictionary
