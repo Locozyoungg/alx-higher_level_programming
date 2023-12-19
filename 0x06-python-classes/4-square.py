61fc1d5ae17b10ce84217faef96f6fd498b63bc6
Locozyoungg <collaustin27@gmail.com>
2023-12-19 12:55:18 -0800

    4_squr

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This module defines a class Square that defines a square"""
+
+
+class Square:
+    """This class represents a square with a private attribute size"""
+
+    def __init__(self, size=0):
+        """This method initializes a square with a given size"""
+        self.size = size
+
+    @property
+    def size(self):
+        """This property returns the current size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """This property setter sets the size of the square"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """This method returns the current square area"""
+        return self.__size ** 2
