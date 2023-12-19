af0d68049703deb218fa4a52ffad6d04e89bc058
Locozyoungg <collaustin27@gmail.com>
2023-12-19 11:49:43 -0800

    1_sqr

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a class Square that defines a square"""
+
+
+class Square:
+    """This class represents a square with a private attribute size"""
+
+    def __init__(self, size):
+        """This method initializes a square with a given size"""
+        self.__size = size
