6c4d43175fef2f73714da98bb5e44015a9aa2d14
Locozyoungg <collaustin27@gmail.com>
2024-01-04 14:05:06 -0800

    101_rect

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python3
+"""
+N Queens problem solver
+"""
+
+import sys
+
+
+def is_safe(board, row, col, n):
+    """
+    Check if it's safe to place a queen in the given position.
+    """
+    # Check the row on the left side
+    for i in range(col):
+        if board[row][i] == 1:
+            return False
+
+    # Check upper diagonal on the left side
+    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    # Check lower diagonal on the left side
+    for i, j in zip(range(row, n), range(col, -1, -1)):
+        if board[i][j] == 1:
+            return False
+
+    return True
+
+
+def solve_nqueens_util(board, col, n, solutions):
+    """
+    Recursive utility function to solve N Queens problem.
+    """
+    if col == n:
+        solutions.append([row[:] for row in board])
+        return
+
+    for i in range(n):
+        if is_safe(board, i, col, n):
+            board[i][col] = 1
+            solve_nqueens_util(board, col + 1, n, solutions)
+            board[i][col] = 0
+
+
+def solve_nqueens(n):
+    """
+    Solve the N Queens problem and print the solutions.
+    """
+    if not isinstance(n, int):
+        print("N must be a number")
+        sys.exit(1)
+
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+
+    board = [[0 for _ in range(n)] for _ in range(n)]
+    solutions = []
+
+    solve_nqueens_util(board, 0, n, solutions)
+
+    for solution in solutions:
+        print(solution)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: {} N".format(sys.argv[0]))
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+        solve_nqueens(N)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
