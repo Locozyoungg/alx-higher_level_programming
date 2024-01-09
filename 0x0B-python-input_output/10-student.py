75e07ab4ca2ae3182943577bc6b40cb98aa4e2bd
Locozyoungg <collaustin27@gmail.com>
2024-01-09 12:58:21 -0800

    stdnt

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""
+Module to provide Student class.
+"""
+
+class Student:
+    """
+    Student class definition.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student instance.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        Args:
+            attrs (list): A list of strings specifying the attributes to retrieve.
+
+        Returns:
+            dict: A dictionary representation of the Student instance.
+        """
+        json_dict = {}
+        if attrs is None:
+            attrs = self.__dict__.keys()
+        for key, value in self.__dict__.items():
+            if key in attrs:
+                json_dict[key] = value
+        return json_dict
+
