861795fe846171eed01adb9815e02506033bd19e
Locozyoungg <collaustin27@gmail.com>
2024-01-15 13:02:11 -0800

    17_base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -41,4 +41,11 @@ class Base:
             return []
         else:
             return json.loads(json_string)
-
+    
+    @classmethod
+    def create(cls, **dictionary):
+        """Class method to return an instance with all attributes already set."""
+        dummy_instance = cls(1, 1)
+        dummy_instance.update(**dictionary)
+        return dummy_instance
+    
