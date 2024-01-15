618d7fc18fa53668eef7bc2928559dfb9f59b8be
Locozyoungg <collaustin27@gmail.com>
2024-01-15 00:13:20 -0800

    13_square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,106 +1,47 @@
 #!/usr/bin/python3
-"""Module defining the Rectangle class."""
+"""Module defining the Square class."""
+from models.rectangle import Rectangle
 
-class Rectangle:
-    """Rectangle class."""
+class Square(Rectangle):
+    """Square class inherits from Rectangle."""
 
-    def __init__(self, width, height, x=0, y=0, id=None):
-        """Rectangle class constructor."""
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-        self.id = id if id is not None else 0
+    def __init__(self, size, x=0, y=0, id=None):
+        """Square class constructor."""
+        super().__init__(size, size, x, y, id)
 
     @property
-    def width(self):
-        """Getter method for width."""
-        return self.__width
+    def size(self):
+        """Getter method for size."""
+        return self.width
 
-    @width.setter
-    def width(self, value):
-        """Setter method for width."""
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """Getter method for height."""
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """Setter method for height."""
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
-        self.__height = value
-
-    @property
-    def x(self):
-        """Getter method for x."""
-        return self.__x
-
-    @x.setter
-    def x(self, value):
-        """Setter method for x."""
-        if not isinstance(value, int):
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
-        self.__x = value
-
-    @property
-    def y(self):
-        """Getter method for y."""
-        return self.__y
-
-    @y.setter
-    def y(self, value):
-        """Setter method for y."""
-        if not isinstance(value, int):
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
-        self.__y = value
-
-    def area(self):
-        """Calculate and return the area of the rectangle."""
-        return self.width * self.height
-
-    def display(self):
-        """Print the rectangle with '#' characters to stdout."""
-        for i in range(self.y):
-            print()
-        for i in range(self.height):
-            print(" " * self.x + "#" * self.width)
-
-    def __str__(self):
-        """Overridden __str__ method to return a formatted string."""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(
-            self.id, self.x, self.y, self.width, self.height)
+    @size.setter
+    def size(self, value):
+        """Setter method for size."""
+        self.width = value
+        self.height = value
 
     def update(self, *args, **kwargs):
-        """Update attributes based on arguments."""
+        """Public method to update attributes."""
+        attr_list = ['id', 'size', 'x', 'y']
+        
         if args:
-            attributes = ["id", "width", "height", "x", "y"]
-            for attr, value in zip(attributes, args):
-                setattr(self, attr, value)
-        elif kwargs:
+            for idx, arg in enumerate(args):
+                setattr(self, attr_list[idx], arg)
+        else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
     def to_dictionary(self):
-        """Return dictionary representation of the rectangle."""
+        """Return dictionary representation of the square."""
         return {
             "id": self.id,
-            "width": self.width,
-            "height": self.height,
+            "size": self.size,
             "x": self.x,
             "y": self.y
         }
+
+    def __str__(self):
+        """Overridden __str__ method to return a formatted string."""
+        return "[Square] ({}) {}/{} - {}".format(
+            self.id, self.x, self.y, self.size)
 
