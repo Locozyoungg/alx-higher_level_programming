138111fedc959b5532a83a7f2f3c5785fe8a6e80
Locozyoungg <collaustin27@gmail.com>
2024-01-15 12:40:08 -0800

    14_base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,15 +1,25 @@
 #!/usr/bin/python3
 """Module defining the Base class."""
-class Base:
-    """This class will be the base of all other classes in this project."""
+import json
 
-    __nb_objects = 0  # private class attribute
+class Base:
+    """Base class for managing id attribute in all future classes."""
+
+    __nb_objects = 0
 
     def __init__(self, id=None):
-        """Class constructor."""
+        """Base class constructor."""
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Static method to return the JSON string representation of list_dictionaries."""
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
 
