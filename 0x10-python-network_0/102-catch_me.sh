ea78ca4b56cd35ce5f6436c26afadd8e69b8e08f
Locozyoungg <collaustin27@gmail.com>
2024-03-27 16:29:22 -0700

    catch_me

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# This script makes a request to 0.0.0.0:5000/catch_me
+# causing the server to respond with a message containing "You got me!" in the body of the response
+
+curl -sX PUT -d "user_id=98" 0.0.0.0:5000/catch_me
