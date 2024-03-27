faff12d1f9f53fbfe60c5cd9768cd5444f6f28e2
Locozyoungg <collaustin27@gmail.com>
2024-03-27 16:27:07 -0700

    post_json

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,15 +1,3 @@
 #!/bin/bash
-# This script sends a JSON POST request to a URL passed as the first argument,
-# with the contents of a file passed as the second argument in the body of the request,
-# and displays the body of the response.
-
-file_content=$(cat "$2")
-
-# Check if the file content is valid JSON
-if ! jq -e . >/dev/null 2>&1 <<<"$file_content"; then
-    echo "Not a valid JSON"
-    exit 1
-fi
-
-# Send POST request and display response body
-curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
+# Sends a JSON POST request to a given URL with a given JSON file.
+curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
